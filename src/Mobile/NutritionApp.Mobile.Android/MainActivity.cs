367912c76c2544b2d29070fd337d387f845cc339
Timo <timo.schwarzer@gmail.com>
2019-10-18 12:22:30 +0200

    Added the splash screen

--- src/Mobile/NutritionApp.Mobile.Android/MainActivity.cs
+++ src/Mobile/NutritionApp.Mobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace NutritionApp.Mobile.Droid
 {
-    [Activity(Label = "NutritionApp.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "NutritionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
